5669c9faceb3a70441e7b714354a638113d58f7c
Bardemic <134565367+Bardemic@users.noreply.github.com>
2023-08-06 04:29:50 -0500

    Add credit for photo, logo

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirmPassword");
+const passwordSpan = document.querySelector("#passwordMatchSpan");
 
 function comparePasswords() {
     if(password.value === confirmPassword.value){
         password.setCustomValidity("");
         confirmPassword.setCustomValidity("");
+        passwordSpan.style.display = "none";
     }
     else{
         password.setCustomValidity("Passwords don't match.");
         confirmPassword.setCustomValidity("Passwords don't match.");
+        passwordSpan.style.display = "block";
     }
 }
 
